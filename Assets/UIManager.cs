a21747f73a64ec963e4796497bcceb6692fe6901
Nestor Bermejillo Gonzalez <n.bermejillo.2021@alumnos.urjc.es>
2025-05-23 16:39:01 +0200

    Initial commit

---
+++ Assets/UIManager.cs
@@ -0,0 +1,49 @@
+using Unity.Netcode;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace HelloWorld
+{
+    public class HelloWorldManager : MonoBehaviour
+    {
+        //private NetworkManager m_NetworkManager;
+
+        void Awake()
+        {
+            //m_NetworkManager = this.GetComponent<NetworkManager>();
+        }
+
+        void OnGUI()
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 300, 300));
+            if (!NetworkManager.Singleton.IsClient && !NetworkManager.Singleton.IsServer)
+            {
+                StartButtons();
+            }
+            else
+            {
+                StatusLabels();
+
+            }
+
+            GUILayout.EndArea();
+        }
+
+        static void StartButtons()
+        {
+            if (GUILayout.Button("Host")) NetworkManager.Singleton.StartHost();
+            if (GUILayout.Button("Client")) NetworkManager.Singleton.StartClient();
+            if (GUILayout.Button("Server")) NetworkManager.Singleton.StartServer();
+        }
+
+        static void StatusLabels()
+        {
+            var mode = NetworkManager.Singleton.IsHost ?
+                "Host" : NetworkManager.Singleton.IsServer ? "Server" : "Client";
+
+            GUILayout.Label("Transport: " +
+                NetworkManager.Singleton.NetworkConfig.NetworkTransport.GetType().Name);
+            GUILayout.Label("Mode: " + mode);
+        }
+    }
+}
