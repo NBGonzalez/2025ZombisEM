d5e160065954e4e5fcf83092f7bc8470af866caa
Nestor Bermejillo Gonzalez <n.bermejillo.2021@alumnos.urjc.es>
2025-05-27 17:10:30 +0200

    Los nombres rotan correctamente en función al jugador de la partida 🐷

--- Assets/NamePlayers.cs
+++ Assets/NamePlayers.cs
@@ -8,11 +8,15 @@ public class NamePlayers : MonoBehaviour
 {
     public GameObject camera;
     public GameObject playerOwner;
+    public List<GameObject> playersNames = new List<GameObject>();
+
+    private Vector3 worldUp;
 
 
     // Start is called before the first frame update
     void Start()
     {
+
         var allPlayers = GameObject.FindGameObjectsWithTag("Player");
         foreach (var player in allPlayers)
         {
@@ -20,7 +24,11 @@ public class NamePlayers : MonoBehaviour
             {
                 playerOwner = player;
                 camera = playerOwner.transform.GetChild(3).gameObject;
-                break;
+                playersNames.Add(player.transform.GetChild(4).gameObject);
+            }
+            else
+            {
+                playersNames.Add(player.transform.GetChild(4).gameObject);
             }
         }
     }
@@ -28,7 +36,10 @@ public class NamePlayers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        //transform.rotation = Quaternion.LookRotation(this.transform.position - camera.transform.position);
+        foreach (var playerName in playersNames)
+        {
+            playerName.transform.LookAt(camera.transform, worldUp);
+            playerName.transform.rotation = Quaternion.Euler(0, playerName.transform.rotation.eulerAngles.y, 0);
+        }
     }
 }
